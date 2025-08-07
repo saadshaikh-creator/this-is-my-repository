0822643a466af9e0fb11aeb89cd00cb821bd6501
saadshaikh-creator <saadshaikh421301@gmail.com>
2025-08-07 13:50:32 +0530

    Add files via upload

---
+++ portjs.js
@@ -0,0 +1,41 @@
+
+function promptLogin() {
+    document.getElementById("loginModal").style.display = "flex";
+}
+
+function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+}
+
+function redirectToLogin() {
+    window.location.href = "login.html"; // Change this if needed
+}
+
+
+// ************************************
+function promptLoginpage() {
+    document.getElementById("loginpage").style.display = "flex";
+}
+
+function closeLoginModalpage() {
+    document.getElementById("loginpage").style.display = "none";
+}
+
+function redirectToLogin() {
+    window.location.href = "login.html"; // Change this if needed
+}
+// ************************************
+
+
+function redirectTohome() {
+    window.location.href = "portfolio.html"; // Change this if needed
+}
+function redirectToServices() {
+    window.location.href = "file:///C:/Users/Saad/my%20port/portfolio.html#service"; // Change this if needed
+}
+function redirectToAboutme() {
+    window.location.href = "file:///C:/Users/Saad/my%20port/portfolio.html#about"; // Change this if needed
+}
+function redirectToContact() {
+    window.location.href = "file:///C:/Users/Saad/my%20port/portfolio.html#contact"; // Change this if needed
+}
